3be0ae48a24fe30fd02ac20bdce273a8508aabf4
Maro Wael <marwanmero386@gmail.com>
2026-02-24 19:04:53 +0200

    Initial commit

---
+++ src/main/java/com/eazybytes/jobportal/company/service/ICompanyService.java
@@ -0,0 +1,9 @@
+package com.eazybytes.jobportal.company.service;
+
+import com.eazybytes.jobportal.dto.CompanyDto;
+
+import java.util.List;
+
+public interface ICompanyService {
+    List<CompanyDto> getAllCompanies();
+}
